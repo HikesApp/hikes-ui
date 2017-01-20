c25b82a9dcb74c99ffda9d9ec847f1b0869c5d39
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2017-01-20 08:41:32 +0000

    added redux dev tools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import hikesApp from './reducers';
 
 const configureStore = () => {
   const middlewares = [thunk];
   if (process.env.NODE_ENV !== 'production') {
+    console.log('NOT PRODUCTION!!!!');
     middlewares.push(createLogger());
   }
 
+  const storeEnhancer = applyMiddleware(...middlewares);
+
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
   return createStore(
     hikesApp,
-    applyMiddleware(...middlewares)
+    process.env.NODE_ENV !== 'production' ? composeWithDevTools(storeEnhancer) : storeEnhancer
   );
 };
 
